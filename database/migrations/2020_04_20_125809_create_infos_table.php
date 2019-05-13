5c3fa18457915bb7a193918c9d61d8e027693f59
saber13812002 <saber.tabatabaee@gmail.com>
2019-05-13 18:30:38 +0430

    2-create-info-table

---
+++ database/migrations/2020_04_20_125809_create_infos_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('infos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('author_id')->nullable();
+
+            $table->string('name')->nullable();
+            $table->string('family')->nullable();
+            $table->string('email')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('national_code')->nullable();
+            $table->string('psn_id')->nullable();
+
+            $table->date('req_date');
+            $table->datetime('req_time');
+            $table->string('req_status')->nullable();
+            $table->timestamps();
+        });
+
+        // DB::table('infos')->insert(
+        //     [
+        //         'author_id' => 1,
+        //         'req_date' => new DateTime(),
+        //         'req_time' => new DateTime()
+        //     ]
+        // );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('infos');
+    }
+}
